71d6b740f9cb31fd76b01c3e4bbd26ca3d204e41
Gabriel Holanda <gabriel_ebot@hotmail.com>
2020-06-23 20:31:23 -0300

    My try on tests, but dependency errors got the best of me

--- src/services/customer/customer.service.spec.ts
+++ src/services/customer/customer.service.spec.ts
@@ -1,18 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CustomerService } from './customer.service';
+import { RedisService } from 'src/redis/redis.service';
 
 describe('CustomerService', () => {
   let service: CustomerService;
+  let redisService: RedisService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CustomerService],
+      providers: [CustomerService, RedisService],
     }).compile();
 
+    redisService = module.get<RedisService>(RedisService);
     service = module.get<CustomerService>(CustomerService);
   });
 
   it('should be defined', () => {
+    expect(redisService).toBeDefined();
     expect(service).toBeDefined();
   });
 });
--- src/services/groove-auth/groove-auth.service.spec.ts
+++ src/services/groove-auth/groove-auth.service.spec.ts
@@ -1,18 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GrooveAuthService } from './groove-auth.service';
+import { HttpService, INestApplication, HttpModule } from '@nestjs/common';
+import { of } from 'rxjs';
+import { AppModule } from 'src/app.module';
 
 describe('GrooveAuthService', () => {
   let service: GrooveAuthService;
+  let httpService: HttpService;
+  let app: INestApplication;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GrooveAuthService],
+      imports: [AppModule, HttpModule],
+      providers: [HttpService, GrooveAuthService],
     }).compile();
 
+    app = module.createNestApplication();
+    httpService = module.get<HttpService>(HttpService);
     service = module.get<GrooveAuthService>(GrooveAuthService);
+    await app.init();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // it('should get token', () => {
+  //   jest.spyOn(httpService, 'get').mockImplementationOnce(() => {
+  //     of(result)
+  //   })
+  // })
 });
