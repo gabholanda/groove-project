6de6257111a17530bb67c243a1187936a3cbfc8c
Gabriel Holanda <gabriel_ebot@hotmail.com>
2020-06-23 20:30:30 -0300

    Update on RedisService to follow NestJS DI design pattern

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,26 +1,30 @@
 import * as Redis from 'ioredis';
 import { Customer } from 'src/models/customer.model';
 import { uuid } from 'uuidv4';
+import { Injectable } from '@nestjs/common';
 
-const redis = new Redis(6379, '0.0.0.0');
 
-const redisMethods =
-{
-    set: (customer: Customer): Customer => {
+@Injectable()
+export class RedisService {
+    redis: any;
+
+    constructor() {
+        this.redis = new Redis(6379, '0.0.0.0');
+    }
+
+    set(customer: Customer): Customer {
         if (customer.id) {
-            redis.set(`customer:${customer.id}`, JSON.stringify(customer));
+            this.redis.set(`customer:${customer.id}`, JSON.stringify(customer));
         } else {
             const customerUuid: string = uuid();
             customer.id = customerUuid;
-            redis.set(`customer:${customerUuid}`, JSON.stringify(customer));
+            this.redis.set(`customer:${customerUuid}`, JSON.stringify(customer));
         }
 
         return customer;
-    },
-    get: async (id: string): Promise<any> => {
-        const response = await redis.get(`customer:${id}`)
+    }
+    async get(id: string): Promise<any> {
+        const response = await this.redis.get(`customer:${id}`)
         return response;
     }
 }
-
-export default redisMethods;
